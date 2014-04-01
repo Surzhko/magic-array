5fa8dd039586bc89a2080ce3f2060828af7e8227
Surzhko Yevhen <SurzhkoYevhen@gmail.com>
2014-04-01 21:41:29 +0300

    initial commit

---
+++ lib/magic-array.rb
@@ -0,0 +1,7 @@
+require 'magic-array/version'
+
+module Magic
+  module Array
+    # Your code goes here...
+  end
+end
---
+++ lib/magic-array/version.rb
@@ -0,0 +1,5 @@
+module Magic
+  module Array
+    VERSION = '0.0.1'
+  end
+end
